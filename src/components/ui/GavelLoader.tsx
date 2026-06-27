583ee1c67e01714a7554358037f10a78f79f6d16
Numan Faisal <numanfaisal980@gmail.com>
2026-06-27 14:00:12 +0530

    Just add img role in svg

--- src/components/ui/GavelLoader.tsx
+++ src/components/ui/GavelLoader.tsx
@@ -16,6 +16,7 @@ export default function GavelLoader({ isThinking = true }: GavelLoaderProps) {
         className="gl-svg"
         viewBox="-3 -3 50 50"
         xmlns="http://www.w3.org/2000/svg"
+        role="img"
       >
         <style>{`
           .gl-arm {
